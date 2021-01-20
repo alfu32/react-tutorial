c3ad78732f36091ce253f0654fe0d75244836cd0
alfu32 <ioan.alferaru@gmail.com>
2021-01-20 12:14:06 +0100

    2 files modified

--- src/Board.js
+++ src/Board.js
@@ -1,3 +1,11 @@
 import React from "react";
 
-export class Board extends React.Component {}
+export class Board extends React.Component {
+  render() {
+    return (
+      <div class="board">
+        <h4>{this.props.title}</h4>
+      </div>
+    );
+  }
+}
--- src/Game.js
+++ src/Game.js
@@ -1,3 +1,9 @@
 import React from "react";
 
-export class Game extends React.Component {}
+export class Game extends React.Component {
+  render() {
+    return <div class="game">
+      <h4>{this.props.title}</h4>
+    </div>;
+  }
+}
