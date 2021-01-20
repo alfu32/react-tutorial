e2867b1a9b49f2ac3f18ec5ee5e391628bbaeac6
alfu32 <ioan.alferaru@gmail.com>
2021-01-20 12:23:43 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-      <Game />
+      <Game title={'coucou'}/>
     </div>
   );
 }
