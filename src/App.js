59e08d611a9a20ae8dddc20e661c7851764e3b2e
alfu32 <ioan.alferaru@gmail.com>
2021-01-20 12:22:31 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./style.css";
-import Game from './Game.js';
-import Board from './Board.js';
-import Square from './Square.js';
+import Game from "./Game.js";
+import Board from "./Board.js";
+import Square from "./Square.js";
 
 export default function App() {
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <Game />
     </div>
   );
 }
--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-export class Game extends React.Component {
+class Game extends React.Component {
   render() {
-    return <div class="game">
-      <h4>{this.props.title}</h4>
-    </div>;
+    return (
+      <div class="game">
+        <h4>{this.props.title}</h4>
+      </div>
+    );
   }
 }
+
+export { Game };
